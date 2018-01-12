8804732c5a0729d608ebe1d2a96410e247f23baf
Benjamin Wilkins <wilkben@yahoo.ca>
2018-01-12 14:18:24 -0500

    added webpage framework

--- Source/collision.h
+++
@@ -1,8 +0,0 @@
-#ifndef collision
-#define collision
-
-int detectCollision(Object obj1, Object obj2);
-Object reactCollision(Object obj1, Object obj2, bool bounce);
-Object reactUpCollision(Object obj1, Object obj2);
-
-#endif
--- Source/file.h
+++
@@ -1,9 +0,0 @@
-#ifndef file
-#define file
-
-extern FONT *fonts[];
-
-int loadFonts();
-void loadLevelData(char levelData[NUM_LEVELS][SCREENH/BLOCKH][SCREENW/BLOCKW], Text onScreenText[NUM_LEVELS][MAX_NUM_LINES]);
-
-#endif
